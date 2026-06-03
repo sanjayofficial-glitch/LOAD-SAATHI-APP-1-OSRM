62b632e2d0aeae49c45030f1d7e404d13eddc2c2
[dyad] <sanjayofficial720@gmail.com>
2026-06-03 10:12:49 +0530

    [dyad] Fixing resizable handle ref error - wrote 1 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
+import { GripVertical } from "lucide-react"
 import { cn } from "@/lib/utils"
 
 const ResizablePanelGroup = React.forwardRef<
@@ -31,22 +32,27 @@ const ResizablePanel = React.forwardRef<
 ))
 ResizablePanel.displayName = "ResizablePanel"
 
-const ResizableHandle = React.forwardRef<
-  HTMLDivElement,
-  React.ComponentPropsWithoutRef<"div"> & {
-    withHandle?: boolean
-  }
->(({ className, withHandle = false, ...props }, ref) => (
+const ResizableHandle = ({
+  withHandle,
+  className,
+  ...props
+}: React.ComponentProps<typeof PanelResizeHandle> & {
+  withHandle?: boolean
+}) => (
   <PanelResizeHandle
-    ref={ref}
     className={cn(
-      "w-px bg-border hover:bg-border/80 transition-colors",
-      withHandle && "w-2 hover:w-3",
+      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0",
       className
     )}
     {...props}
-  />
-))
+  >
+    {withHandle && (
+      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-slate-800">
+        <GripVertical className="h-2.5 w-2.5 text-slate-400" />
+      </div>
+    )}
+  </PanelResizeHandle>
+)
 ResizableHandle.displayName = "ResizableHandle"
 
 export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
