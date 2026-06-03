6c189ffebaf85206a8b1527d6dbee56921048e21
[dyad] <sanjayofficial720@gmail.com>
2026-06-03 10:09:26 +0530

    [dyad] Fix TypeScript errors in resizable.tsx and MonitoringDashboard.tsx

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,33 +1,52 @@
+"use client"
+
 import * as React from "react"
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels"
 import { cn } from "@/lib/utils"
 
-/**
- * Resizable wrapper for panel elements.
- * Provides resizing handles while preserving original structure.
- */
-const Resizable = React.forwardRef<
-  React.ElementRef<typeof React>,
-  React.ComponentPropsWithoutRef<"div"> // Generic div props
->(({ className, children, ...props }, ref) => (
-  <div
+const ResizablePanelGroup = React.forwardRef<
+  React.ElementRef<typeof PanelGroup>,
+  React.ComponentPropsWithoutRef<typeof PanelGroup>
+>(({ className, ...props }, ref) => (
+  <PanelGroup
     ref={ref}
-    className={cn("div", className)}
+    className={cn("flex h-full w-full data-[panel-direction=vertical]:flex-col", className)}
     {...props}
   />
 ))
+ResizablePanelGroup.displayName = PanelGroup.displayName
 
-const ResizableHandle = React.forwardRef<
-  React.ElementRef<"div">,
-  React.ComponentPropsWithoutRef<"div">
+const ResizablePanel = React.forwardRef<
+  React.ElementRef<typeof Panel>,
+  React.ComponentPropsWithoutRef<typeof Panel>
 >(({ className, ...props }, ref) => (
-  <div
+  <Panel
     ref={ref}
     className={cn(
-      "flex h-4 w-full items-center justify-center rounded-sm bg-border hover:bg-accent transition-colors",
+      "flex h-full w-full data-[panel-direction=vertical]:flex-col",
       className
     )}
     {...props}
   />
 ))
+ResizablePanel.displayName = Panel.displayName
 
-export { Resizable, ResizableHandle }
+const ResizableHandle = React.forwardRef<
+  React.ElementRef<typeof PanelResizeHandle>,
+  React.ComponentPropsWithoutRef<typeof PanelResizeHandle> & {
+    withHandle?: boolean
+  }
+>(({ className, withHandle = false, ...props }, ref) => (
+  <PanelResizeHandle
+    ref={ref}
+    className={cn(
+      "w-px bg-border hover:bg-border/80 transition-colors",
+      withHandle && "w-2 hover:w-3",
+      className
+    )}
+    {...props}
+  />
+))
+ResizableHandle.displayName = PanelResizeHandle.displayName
+
+export { ResizablePanelGroup, ResizablePanel, ResizableHandle }
