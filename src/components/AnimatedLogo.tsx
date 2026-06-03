cae72891062211d383e3f976a45826f5b017f053
[dyad] <sanjayofficial720@gmail.com>
2026-06-03 07:46:59 +0530

    [dyad] wrote 1 file(s)

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -8,8 +8,8 @@ const AnimatedLogo = () => {
     <div className="relative w-24 h-24 mx-auto">
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="relative">
-          {/* Truck icon with rotation animation */}
-          <div className="w-16 h-16 bg-orange-600 rounded-2xl flex items-center justify-center shadow-lg animate-spin">
+          {/* Truck icon without rotation animation */}
+          <div className="w-16 h-16 bg-orange-600 rounded-2xl flex items-center justify-center shadow-lg">
             <Truck className="h-10 w-10 text-white" />
           </div>
           {/* Pulsing ring */}
