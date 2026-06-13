89dc07707d4e7c92e70907cd195de1a78cad614d
[dyad] <sanjayofficial720@gmail.com>
2026-06-13 18:27:40 +0530

    Update 3 files

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./globals.css";
 
 async function mountApp() {
@@ -11,6 +13,8 @@ async function mountApp() {
     root.render(
       <React.StrictMode>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </React.StrictMode>
     );
   } catch (err) {
